746bb82f4d6a6738118d0301bdb23b33f6291533
Riva Almero <rvalms@icloud.com>
2025-02-15 23:00:15 +0700

    feat: auth

---
+++ app/Http/Controllers/AuthC.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthR;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthC extends Controller
+{
+    public function login(AuthR $request)
+    {
+        return $this->response($request->ip());
+        $payload = $request->validated();
+        $user = User::where('email', $payload['email'])->firstOrFail();
+        if (!Hash::check($payload['password'], $user->password)) {
+            throw new \Exception('Invalid credentials', 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->response([
+            'user' => $user,
+            'token' => $token
+        ], 'Login success');
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        return $this->response($user->currentAccessToken()->delete(), 'Logout success');
+    }
+
+    public function me(Request $request)
+    {
+        return $this->response($request->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,29 @@
 <?php
 
 use App\Http\Controllers\AiReviewC;
+use App\Http\Controllers\AuthC;
 use App\Http\Controllers\QuestionC;
 use App\Http\Controllers\ResponseC;
 use App\Http\Controllers\ReviewC;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('/auth')->controller(AuthC::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', 'logout');
+        Route::get('me', 'me');
+    });
+});
+
 Route::prefix('/questions')->controller(QuestionC::class)->group(function () {
     Route::get('/', 'list');
     Route::get('/{id}', 'find');
-    Route::post('/', 'create');
-    Route::put('/{id}', 'update');
-    Route::delete('/{id}', 'delete');
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::post('/', 'create');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'delete');
+    });
 });
 
 Route::prefix('/responses')->controller(ResponseC::class)->group(function () {
@@ -20,19 +31,25 @@ Route::prefix('/responses')->controller(ResponseC::class)->group(function () {
     Route::get('/{id}', 'find');
     Route::post('/', 'create');
     Route::put('/{id}', 'update');
-    Route::delete('/{id}', 'delete');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::delete('/{id}', 'delete');
+    });
 });
 
 Route::prefix('/reviews')->controller(ReviewC::class)->group(function () {
     Route::get('/', 'list');
     Route::get('/{id}', 'find');
-    Route::post('/', 'create');
-    Route::put('/{id}', 'update');
-    Route::delete('/{id}', 'delete');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', 'create');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'delete');
+    });
 });
 
 Route::prefix('/ai-reviews')->controller(AiReviewC::class)->group(function () {
     Route::get('/', 'list');
     Route::get('/{id}', 'find');
-    Route::delete('/{id}', 'delete');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::delete('/{id}', 'delete');
+    });
 });
