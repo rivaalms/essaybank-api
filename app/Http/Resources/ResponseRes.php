fa2ec0942e800f5580ca5d95f820b89426902ab5
Riva Almero <rvalms@icloud.com>
2025-03-02 21:21:34 +0700

    feat: review count on response

--- app/Http/Resources/ResponseRes.php
+++ app/Http/Resources/ResponseRes.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\AiReview;
+use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,13 +17,25 @@ class ResponseRes extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'                => $this->id,
-            'question_id'       => $this->question_id,
-            'question'          => $this->whenLoaded('question', fn () => $this->question->question),
-            'reference_answer'  => $this->whenLoaded('question', fn () => $this->question->reference_answer),
-            'answer'            => $this->answer,
-            'created_at'        => $this->created_at,
-            'updated_at'        => $this->updated_at
+            'id'                        => $this->id,
+            'question_id'               => $this->question_id,
+            'question'                  => $this->whenLoaded('question', fn () => $this->question->question),
+            'reference_answer'          => $this->whenLoaded('question', fn () => $this->question->reference_answer),
+            'answer'                    => $this->answer,
+            'total_reviews'             => $this->when($request->boolean('review_count'), $this->loadReviewCount($this->id)),
+            'total_ai_reviews'          => $this->when($request->boolean('review_count'), $this->loadAiReviewCount($this->id)),
+            'created_at'                => $this->created_at,
+            'updated_at'                => $this->updated_at
         ];
+    }
+
+    private function loadReviewCount($id)
+    {
+        return Review::where('response_id', $id)->count();
+    }
+
+    private function loadAiReviewCount($id)
+    {
+        return AiReview::where('response_id', $id)->count();
     }
 }
